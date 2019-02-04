d7b990809f5b850e084fae695a6ff76336d5a880
Alexandr Kustov <appstore.ios@yandex.ru>
2019-02-04 15:57:25 +0300

    исправления

--- index.php
+++ index.php
@@ -1,102 +1,83 @@
+
+
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Домашка</title>
+    <h1>1 задание</h1>
+</head>
+<body>
+
 <?php
 
+const operation_and = '&&';
+const operation_or = '||';
+const operation_xor = 'XOR';
 
 
+function a_and_b($a, $b, $c) {
 
-// 1 Задание
-echo '1.1 (&&)';
-echo '<br>';
-
-echo '╔═╦══╦═══╗';
-echo '<br>';
-echo '║ a ║ b ║ && ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 0 ║ 0 ║ 0 ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 1 ║ 0 ║ 0 ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 0 ║ 1 ║ 0 ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 1 ║ 1 ║ 1 ║';
-echo '<br>';
-echo '╚═╩══╩═══╝';
+    if ($c == operation_and) {
+        if ($a == $b) {
+            return $a .' '. $c .' '. $b . ' = ' . 1;
+        } else {
+            return $a .' '. $c .' '. $b . ' = ' . 0;
+        }
+    }
 
 
-echo '<br>';
-echo '<br>';
-echo '<br>';
+    if ($c == operation_or) {
+        if ($a == $b) {
+            return $a .' '. $c .' '. $b . ' = ' . 0;
+        } else {
+            return $a .' '. $c .' '. $b . ' = ' . 1;
+        }
+    }
 
-echo '1.2 (||)';
-echo '<br>';
-
-echo '╔═╦══╦═══╗';
-echo '<br>';
-echo '║ a ║ b ║ || ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 0 ║ 0 ║ 0 ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 1 ║ 0 ║ 1 ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 0 ║ 1 ║ 1 ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 1 ║ 1 ║ 1 ║';
-echo '<br>';
-echo '╚═╩══╩═══╝';
+    if ($c == operation_xor) {
+        if ($a == $b) {
+            return $a .' '. $c .' '. $b . ' = ' . 0;
+        } else {
+            return $a .' '. $c .' '. $b . ' = ' . 1;
+        }
+    }
 
 
-echo '<br>';
-echo '<br>';
-echo '<br>';
+}
+$a = 1;
+$b = 0;
+?>
 
 
-echo '1.3 (XOR)';
-echo '<br>';
+<?php echo a_and_b($a,$b, operation_and); ?>
 
-echo '╔═╦══╦═══╗';
-echo '<br>';
-echo '║ a ║ b ║ xor ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 0 ║ 0 ║ 0 ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 1 ║ 0 ║ 1 ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 0 ║ 1 ║ 1 ║';
-echo '<br>';
-echo '╠═╬══╬═══╣';
-echo '<br>';
-echo '║ 1 ║ 1 ║ 0 ║';
-echo '<br>';
-echo '╚═╩══╩═══╝';
+<?php echo a_and_b($a,$b, operation_or); ?>
+
+<?php echo a_and_b($a,$b, operation_xor); ?>
+
+</body>
+</html>
 
 
-echo '<br>';
-echo '<br>';
-echo '<br>';
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Домашка</title>
+    <h1>2 задание</h1>
+</head>
+<body>
 
-echo '2 Задание';
-echo '<br>';
+
+<?php
 
 function searchDisc($a, $b, $c) {
     $d = ($b * 2) - (4 * $a * $c);
@@ -109,9 +90,7 @@ assert(-76 == searchDisc(10,2,2));
 $pp = searchDisc(10,-2,-2);
 echo $pp;
 
-echo '<br>';
-echo '<br>';
-echo '<br>';
+
 
 function squareRoots($a, $b, $c) {
     $disc = searchDisc($a, $b, $c);
@@ -119,47 +98,97 @@ function squareRoots($a, $b, $c) {
     if (0 < $disc) {
         $x1 = (-$b + sqrt($disc)) / (2 * $a);
         $x2 = (-$b - sqrt($disc)) / (2 * $a);
-        echo 'x1= ' . $x1;
-        echo '<br>';
-        echo 'x2= ' . $x2;
+
+        return 'x1= ' . $x1 . ' , ' . 'x2= ' . $x2;
     } elseif (0 == $disc) {
         $x = (-$b / 2 * $a);
-        echo  'x = ' . $x;
+
+        return 'x = ' . $x;
     } elseif (0 > $disc) {
-        echo 'Уравнение не имеет решений в действительных числах.';
+
+        return 'Уравнение не имеет решений в действительных числах.';
     }
 }
 
 $root = squareRoots(15,-10,-100);
 echo $root;
 
-echo '<br>';
-echo '<br>';
-echo '<br>';
-
-echo '3 Задание';
-echo '<br>';
+?>
+</body>
+</html>
 
 
-$f = include __Dir__ . '/test.php';
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Домашка</title>
+    <h1>3 задание</h1>
+</head>
+<body>
 
-var_dump($f);
+<?php
 
-/*
- * возвращает bool value. Если файл не найдется то вернется false, если путь получится построить, то вернет true (1)
- * попробуйте в путь добавить лишнюю буквы и вы увидите warning и false
- */
+$path = include __Dir__ . '/test.php';
 
+if ( $path ) {
+    echo 'path correct';
+} else {
+    echo 'path incorrect';
+}
 
-echo '<br>';
-echo '<br>';
-echo '<br>';
-
-echo '4 Задание';
-echo '<br>';
+var_dump($path); // путь найден, выводит true
 
 
+$path_2 = include __DIR__ . '/tes.php';
+var_dump($path_2); // путь не найден, выводит false и Warning
+// если путь сломанный, то вернет false и warning и продолжит выполнять код
 
+
+// при успешном подключение php файла, выполняется код в том файле. до оператора return
+
+// оператор return может вернуть значение. например
+// $a = [10,20,30];
+// return $a;
+
+// в файле из которого мы вызывали include мы сможем получить
+// $path = include __DIR__ . '/file.php'
+// $element = $path[0]
+// мы получили в нашем файле элемент из массива array c 0 идексом
+
+
+
+$path_test2 = include __DIR__ . '/test2.php';
+$test2_array = $path_test2['list'];
+var_dump($test2_array);
+
+echo $test2_array[1];
+
+
+
+?>
+
+</body>
+</html>
+
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Домашка</title>
+    <h1>4 задание</h1>
+</head>
+<body>
+
+
+<?php
 function search_gender($name) {
     $last = mb_substr($name,-1,10,'UTF-8');
 
@@ -179,11 +208,18 @@ assert('woman' === search_gender('айгуля'));
 assert('man' === search_gender('альберт'));
 assert(null === search_gender('манвел'));
 
-$person = search_gender('саша');
-echo $person;
-echo '<br>';
 $person2 = search_gender('николай');
 echo $person2;
+
+?>
+</body>
+</html>
+
+
+
+
+
+
 
 
 
